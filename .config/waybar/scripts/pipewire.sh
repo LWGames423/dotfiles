cd7f8cfaab50f4c0df88f982cbbad890231b0d9f
Leo Wang <leowang0423@gmail.com>
2024-07-01 22:08:18 -0400

    latex dots

--- .config/waybar/scripts/pipewire.sh
+++ .config/waybar/scripts/pipewire.sh
@@ -6,12 +6,12 @@ volume=$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | \
 
 if [ $(echo $volume | grep "MUTED") -eq "" ] || [ $(echo $volume) -ne 0 ]; then 
     if [[ $volume -le 100 && $volume -gt 50 ]]; then
-        echo " $volume%"
+        echo "󰕾 $volume%"
     elif [[ $volume -le 50 && $volume -gt 25 ]]; then
-        echo " $volume%"
+        echo "󰖀 $volume%"
     elif [[ $volume -le 25 && $volume -gt 0 ]]; then
-        echo " $volume%"
+        echo "󰕿 $volume%"
     fi
 else
-    echo "MUTE"
+    echo "󰖁"
 fi
