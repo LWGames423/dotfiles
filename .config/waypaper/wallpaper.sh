5f5333d8f57ffb238fc2df79b9053d9dc9e9260c
Leo Wang <leowang0423@gmail.com>
2025-07-14 00:07:39 -0400

    migrated waybar conf to matugen, other tweaks

--- .config/waypaper/wallpaper.sh
+++ .config/waypaper/wallpaper.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
-sleep 1
+for _ in {1..5}; do
+    mapfile -t images < <(swww query | sed -n 's/.*image: \(.*\)$/\1/p')
+    (( ${#images[@]} )) && break
+    sleep 1
+done
+[[ ${#images[@]} -eq 0 ]] && { echo "No wallpaper found"; exit 1; }
 
-WALLPAPER=$(swww query | sed -n 's/.*image: \(.*\)$/\1/p')
+WALLPAPER=${images[0]}   # choose the first monitor’s wallpaper
 
-wal -i $WALLPAPER -e -n -s -t
+# wal -i $WALLPAPER -e -n -s -t
+
 matugen image $WALLPAPER
-ln -sf ~/.cache/wal/colors-kitty.conf ~/.config/kitty/16-colors.conf
-~/.config/matugen/papirus-folders/change-icons.sh
+
+# ln -sf ~/.cache/wal/colors-kitty.conf ~/.config/kitty/16-colors.conf
+
+# ~/.config/matugen/papirus-folders/change-icons.sh
